68ce0fe8760fa38669873e8407dd16ec56e97416
manaten <manaten@manaten.net>
2015-12-02 16:00:13 +0900

    feat(npmrc): † add .npmrc †

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for i in .bashrc .gitconfig .gitmessage .globalignore .screenrc .tmux.conf .vimrc .zshrc
+for i in .bashrc .gitconfig .gitmessage .globalignore .screenrc .tmux.conf .vimrc .zshrc .npmrc
 do
   ln -fs `pwd`/$i ~/$i
 done
