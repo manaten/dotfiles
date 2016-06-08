d3ad8c14ea4f9ea1ca675107da2e8533cfd445a9
manaten <manaten@manaten.net>
2016-06-08 21:31:04 +0900

    feat(direnvrc): † add .direnvrc †

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for i in .bashrc .gitconfig .gitmessage .globalignore .screenrc .tmux.conf .vimrc .zshrc .npmrc
+for i in .bashrc .gitconfig .gitmessage .globalignore .screenrc .tmux.conf .vimrc .zshrc .npmrc .direnvrc
 do
   ln -fs `pwd`/$i ~/$i
 done
