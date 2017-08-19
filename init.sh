8e493cc375d4a6de68e935dde0fd4d80ab7e0fbf
manaten <manaten@manaten.net>
2017-08-20 03:16:04 +0900

    feat(.yarnrc): † add .yarnrc †

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for i in .bashrc .gitconfig .gitmessage .gitignore.global .gitattributes.global .tmux.conf .vimrc .zshrc .direnvrc .editorconfig
+for i in .yarnrc .bashrc .gitconfig .gitmessage .gitignore.global .gitattributes.global .tmux.conf .vimrc .zshrc .direnvrc .editorconfig
 do
   ln -fs `pwd`/$i ~/$i
 done
