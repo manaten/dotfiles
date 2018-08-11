dbe777413a8a91de976026e474ca9a83661a0b14
manaten <manaten@manaten.net>
2018-08-11 21:42:44 +0900

    feat(hyper): add .hyper.js

---
+++ .hyper.js
@@ -0,0 +1,71 @@
+// See https://hyper.is#cfg for all currently supported options.
+module.exports = {
+  config: {
+    updateChannel: 'stable',
+    modifierKeys: {
+      altIsMeta: true
+    },
+
+    fontSize: 14,
+    fontFamily: 'Consolas, "Myrica M", "Lucida Console", monospace',
+    fontWeight: 'normal',
+    fontWeightBold: 'bold',
+
+    cursorColor: 'rgba(248,28,229,0.8)',
+    cursorAccentColor: '#000',
+    cursorShape: 'BLOCK',
+    cursorBlink: false,
+
+    foregroundColor: '#fff',
+    backgroundColor: '#000',
+    selectionColor: 'rgba(248,28,229,0.3)',
+    borderColor: '#333',
+
+    css: '',
+    termCSS: '',
+
+    showHamburgerMenu: '',
+    showWindowControls: '',
+
+    padding: '0',
+
+    colors: {
+      black: '#000000',
+      red: '#C51E14',
+      green: '#1DC121',
+      yellow: '#C7C329',
+      blue: '#0A2FC4',
+      magenta: '#C839C5',
+      cyan: '#20C5C6',
+      white: '#C7C7C7',
+      lightBlack: '#686868',
+      lightRed: '#FD6F6B',
+      lightGreen: '#67F86F',
+      lightYellow: '#FFFA72',
+      lightBlue: '#6A76FB',
+      lightMagenta: '#FD7CFC',
+      lightCyan: '#68FDFE',
+      lightWhite: '#FFFFFF',
+    },
+
+    shell: 'C:\\Windows\\System32\\bash.exe',
+    shellArgs: ['--login', '-c', 'zsh'],
+    env: {},
+
+    bell: 'SOUND',
+    copyOnSelect: false,
+    defaultSSHApp: true,
+  },
+
+  plugins: [
+    'hyper-material-theme'
+  ],
+
+  localPlugins: [],
+
+  keymaps: {
+    "tab:jump:prefix": "",
+    "editor:movePreviousWord": "",
+    "editor:moveNextWord": "",
+  },
+};
